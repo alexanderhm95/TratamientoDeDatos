72a9906119f6f8dc854aed5587323923c03e4a9a
Luis Eduardo Chiles <luis.chiles@avp-ec.com>
2026-02-22 22:19:43 -0500

    servicio para crear usuario

--- user/service.py
+++ user/service.py
@@ -25,3 +25,24 @@ def login_user(username, password):
         raise e
 
     
+def create_user(username, email, password, role='user'):
+    """Crea un nuevo usuario en la base de datos."""
+    try:
+        if User.query.filter_by(username=username).first():
+            raise ValueError('El nombre de usuario ya existe.')
+
+        if User.query.filter_by(email=email).first():
+            raise ValueError('El correo electrónico ya está registrado.')
+
+        # Verificacion de email
+        if email.count('@') != 1 or email.rfind('.') < email.find('@'):
+            raise ValueError('El correo electrónico no es válido.')
+
+        user = User(username=username, email=email, password=password, role=role)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    except Exception as e:
+        db.session.rollback()  # Revertir la transacción en caso de error
+        raise e
